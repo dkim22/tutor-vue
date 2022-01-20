b2646c482754af74585910c0ecc83e0f3a26ae3f
dkim22 <d-kim@adventure-inc.co.jp>
2022-01-20 10:41:07 +0900

    feat(scss): add media query

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
 </template>
 
 <style lang="scss">
+@import "~normalize.css";
+:root {
+  --main-color: hotpink;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>HelloWorld</div>
+  <form class="login-form">
+    <input class="login-form__id" />
+    <input type="password" class="login-form__password" />
+    <input type="button" value="로그인" class="login-form__submit" />
+  </form>
 </template>
 
 <script lang="ts">
@@ -10,4 +14,27 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+.login-form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+
+  @media (max-width: 992px) {
+    flex-direction: column;
+  }
+
+  &__id,
+  &__password,
+  &__submit {
+    @media (max-width: 992px) {
+      height: 30px;
+    }
+  }
+
+  &__submit {
+    background-color: var(--main-color);
+  }
+}
+</style>
